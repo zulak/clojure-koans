e51cbf29b13555f627a79cd90f3fbd6252048966
Matt Zulak <zulak@users.noreply.github.com>
2014-10-18 19:59:04 -0700

    The result of spending a few hours working on clojure-koans.

--- src/koans/09_runtime_polymorphism.clj
+++ src/koans/09_runtime_polymorphism.clj
@@ -10,19 +10,19 @@
                    "!")))
 
 (defmulti diet (fn [x] (:eater x)))
-(defmethod diet :herbivore [a] __)
-(defmethod diet :carnivore [a] __)
-(defmethod diet :default [a] __)
+(defmethod diet :herbivore [{:keys [name]}] (str name " eats veggies."))
+(defmethod diet :carnivore [{:keys [name]}] (str name " eats animals."))
+(defmethod diet :default [{:keys [name]}] (str "I don't know what " name " eats."))
 
 (meditations
   "Some functions can be used in different ways - with no arguments"
-  (= __ (hello))
+  (= "Hello World!" (hello))
 
   "With one argument"
-  (= __ (hello "world"))
+  (= "Hello, you silly world." (hello "world"))
 
   "Or with many arguments"
-  (= __
+  (= "Hello to this group: Peter, Paul, Mary!"
      (hello "Peter" "Paul" "Mary"))
 
   "Multimethods allow more complex dispatching"
--- src/koans/13_recursion.clj
+++ src/koans/13_recursion.clj
@@ -3,21 +3,28 @@
 
 (defn is-even? [n]
   (if (= n 0)
-    __
-    (___ (is-even? (dec n)))))
+    true
+    (not (is-even? (dec n)))))
 
 (defn is-even-bigint? [n]
   (loop [n   n
          acc true]
     (if (= n 0)
-      __
+      acc
       (recur (dec n) (not acc)))))
 
 (defn recursive-reverse [coll]
-  __)
+  (loop [[head & tail] coll acc '()]
+    (if-not head
+      acc
+      (recur tail (conj acc head))
+      )))
 
 (defn factorial [n]
-  __)
+  (loop [step n acc 1]
+    (if (= step 0)
+      acc
+      (recur (dec step) (* step acc)))))
 
 (meditations
   "Recursion ends with a base case"
